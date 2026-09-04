7ceb51765998dd67ff368a6f3e0222212547e3be
Jedi P <2601659@sit.singaporetech.edu.sg>
2026-09-04 14:36:14 +0800

    Third Post

--- hello.py
+++ hello.py
@@ -10,3 +10,14 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+followers = 100
+
+followers += 50
+print("Day1:", followers)
+
+followers += 20
+print("Day2:", followers)
+
+followers -= 10
+print("Day3:", followers)
